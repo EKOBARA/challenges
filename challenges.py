4e24328057f84b8e2e157c4fcd4295bb8953a722
ekobara <emmanuelkobara@gmail.com>
2021-10-20 10:40:20 -0400

    2nd problem complete

--- challenges.py
+++ challenges.py
@@ -9,15 +9,17 @@ def double_char(str):
     return ''.join(wow)
 
 print(double_char("1234!_ "))
+
 # Sum of positive 8 kyu
 # You get an array of numbers, return the sum of all of the positives ones.
 # Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
 
 def positive_sum(arr):
-    return 'Your Answer'
+    retval = [i for i in arr if i > 0]
+    return sum(retval)
 
-
+print(positive_sum([1, -4, 7, 12]))
 # Minimum Steps 7 kyu
 # Given an array of N integers, you have to find how many times you have to add up the smallest numbers in the array until their Sum becomes greater or equal to K.
 # minimumSteps([1, 10, 12, 9, 2, 3], 6)  ==>  return 2
